c708c18d2436009191d96f068c20d4ca7761f585
Steven Cyb <steven.cyb@gmail.com>
2023-01-30 18:35:13 +0000

    feat: implement `part 2`

--- main.go
+++ main.go
@@ -15,11 +15,26 @@ func main() {
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimRight(input, "\n")
 
-		switch input {
-		case ".exit":
-			return
+		// meta-commands
+		if input[0] == '.' {
+			switch input {
+			case ".exit":
+				return
+			default:
+				fmt.Printf("Unrecognized command '%s'.\n", input)
+			}
+
+			continue
+		}
+
+		// statements
+		switch {
+		case strings.HasPrefix(input, "insert "):
+			fmt.Println("This is where we would do an insert.")
+		case strings.HasPrefix(input, "select "):
+			fmt.Println("This is where we would do an select.")
 		default:
-			fmt.Printf("Unrecognized command '%s'.\n", input)
+			fmt.Printf("Unrecognized keyword at start of '%s'.\n", input)
 		}
 	}
 }
